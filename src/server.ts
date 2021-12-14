9fcbb09ed37ab0937af2ccc0bbd40b0b9157b4b2
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2021-12-14 09:38:39 -0300

    instalação do banco usando typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,24 @@
 import express from "express";
+import "./database";
 
 const app = express();
+
+/**
+ * GET = buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = deletar
+ * PATCH = Alterar uma infoação específica
+ */
+
+app.get("/", (request, response) => {
+  return response.json({
+      message: "Olá Bianca, continue assim :D",
+  });  
+});
+
+app.post("/users", (request, response) =>  {
+    return response.json({ message: "Usuário salvo com sucesso!" });
+})
 
 app.listen(3333,  () => console.log("Server is running on port 3333"));
