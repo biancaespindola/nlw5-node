15bfd64aa12d0809d0acaa6dc56471d0ea309467
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-01-02 21:44:07 -0300

    criando a conexão websocket e validação de mensagem de mesmo email

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,51 @@
 import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
+import { UsersService } from '../services/UsersService';
+import { MessagesService } from '../services/MessagesService';
+
+interface IParams {
+	text: string;
+	email: string;
+}
 
 io.on('connect', (socket) => {
-	socket.on('client_first_access', (params) => {
-		console.log(params);
+	const connectionsService = new ConnectionsService();
+	const userService = new UsersService();
+	const messagesServices = new MessagesService();
+
+	socket.on('client_first_access', async (params) => {
+		const socket_id = socket.id;
+		const { text, email } = params;
+		let user_id = null;
+
+		const userExists = await userService.findByEmail(email);
+
+		if (!userExists) {
+			const user = await userService.create(email);
+
+			await connectionsService.create({
+				socket_id,
+				user_id: user.id,
+			});
+			user_id = user_id;
+		} else {
+			user_id = userExists.id;
+			const connection = await connectionsService.findByUserId(
+				userExists.id
+			);
+			if (!connection) {
+				await connectionsService.create({
+					socket_id,
+					user_id: userExists.id,
+				});
+			} else {
+				connection.socket_id = socket_id;
+				await connectionsService.create(connection);
+			}
+		}
+		await messagesServices.create({
+			text,
+			user_id,
+		});
 	});
 });
